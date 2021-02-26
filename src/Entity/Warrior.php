4a6dbd188b5ef299e70b1d337121def57f2e6ae3
JgPhil <jamingph@gmail.com>
2021-02-26 10:53:31 +0100

    initial commit

---
+++ src/Entity/Warrior.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Entity;
+
+
+abstract class Warrior
+{
+    protected $name;
+    protected $health = 100;
+    protected $plague = false;
+    protected $degats;
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+
+    /**
+     * Get the value of health
+     */
+    public function getHealth()
+    {
+        return $this->health;
+    }
+
+    /**
+     * Set the value of health
+     *
+     * @return  self
+     */
+    public function setHealth($health)
+    {
+        $this->health = $health;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of name
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @return  self
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of plague
+     */
+    public function getPlague()
+    {
+        return $this->plague;
+    }
+
+    /**
+     * Set the value of plague
+     *
+     * @return  self
+     */
+    public function setPlague()
+    {
+        $this->plague = true;
+        return $this;
+    }
+
+
+    public function getDamage(int $degats)
+    {
+        $this->health -= $degats;
+        return $this;
+    }
+}
